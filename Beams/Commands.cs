6c3ea140a8c7ece2ecfa5768eb12e208424af1a3
Mick Duprez <mickduprez@gmail.com>
2016-12-19 17:27:08 +1100

    Add sample editing command.

--- Beams/Commands.cs
+++ Beams/Commands.cs
@@ -26,7 +26,8 @@ namespace Beams {
 
             string assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
             ed.WriteMessage("\nBeams application version:{0} loaded successfully!", assemblyVersion);
-            ed.WriteMessage("\nEnter \"BEAM1\" to draw a beam.\n");
+            ed.WriteMessage("\nEnter \"BEAM1\" to draw a beam.");
+            ed.WriteMessage("\nEnter \"ENT-COLOUR\" to change and entity's colour.");
         }
 
         public void Terminate() {
@@ -34,7 +35,7 @@ namespace Beams {
         }
 
         /// <summary>
-        /// Loads the ADACX Main Form
+        /// Creates a simple rectangular beam view
         /// </summary>
         [_AcRx.CommandMethod("BEAM1")]
         public static void CreateBeam() {
@@ -78,6 +79,38 @@ namespace Beams {
             BeamData beamData = new BeamData(200, 120, 6000, p0, p1, pside);
             BeamDrawer beamDrawer = new BeamDrawer(beamData);
             beamDrawer.DrawBeam();
+        }
+
+        /// <summary>
+        /// Example of selecting and editing a drawing entity
+        /// </summary>
+        [_AcRx.CommandMethod("ENT-COLOUR")]
+        public static void ChangeEntColour() {
+            //
+            // Get some user input:
+            //
+            _AcEd.Editor ed = _AcAp.Application.DocumentManager.MdiActiveDocument.Editor;
+            _AcEd.PromptEntityResult res = ed.GetEntity("Select an entity to change its colour:");
+
+            if(res.Status == _AcEd.PromptStatus.OK) {
+                var entId = res.ObjectId;
+
+                var db = _AcDb.HostApplicationServices.WorkingDatabase;
+                using (var tr = db.TransactionManager.StartTransaction()) {
+                    var ent = tr.GetObject(entId, Teigha.DatabaseServices.OpenMode.ForWrite) as _AcDb.Entity;
+
+                    //
+                    // Ask the user for the new colour number
+                    //
+                    _AcEd.PromptIntegerResult intres = ed.GetInteger("\nEnter new colorindex integer (0 -> 255): ");
+
+                    if(intres.Status == _AcEd.PromptStatus.OK) {
+                        ent.ColorIndex = intres.Value;
+                    }
+                    tr.Commit();
+                }
+            }
+            
         }
 
     }
